54801c353f4c326ab0e8f362634035f1d5991adf
FouedDadi <foued.dadii@gmail.com>
2020-08-21 09:28:05 +0100

    fix delete node

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,3 +1,4 @@
+
 #include "lists.h"
 #include <stdlib.h>
 
@@ -15,23 +16,41 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp;
 	unsigned int p;
 
-    if (*head == NULL)
-    return (-1);
-    tmp = *head;
-    if (index == 0)
-    {
-    *head = tmp->next;
-    free(tmp);
-    return (1);
-    }
-    for (p = 0; p < (index - 1) && tmp != NULL; p++)
-    tmp = tmp->next;
-    if (tmp == NULL)
-    return (-1);
-    if (tmp->next == NULL)
-    return (-1);
-    saved_head = tmp->next->next;
-    free(tmp->next);
-    tmp->next = saved_head;
-    return (1);
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
+	{
+		*head = (*head)->next;
+		p++;
+	}
+	if (p != index)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
+	else
+	{
+		if ((*head)->next)
+        {
+            (*head)->prev->next = (*head)->next;
+			(*head)->next->prev = (*head)->prev;
+        }
+        free (*head);
+		*head = saved_head;
+	}
+	return (1);
 }
